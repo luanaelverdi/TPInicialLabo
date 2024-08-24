da2d0c3f2f02154bf24394a05a1b89c077e7b5d7
melmorienega <melimorienega@gmail.com>
2024-08-24 16:22:19 -0300

    codigo para conectarse a mariadb

---
+++ src/public/js/dbconnector.js
@@ -0,0 +1,32 @@
+const mariadb = require('mariadb')
+
+const config = {
+    host: '',
+    user: '',
+    password: '',
+    database: '',
+    connectionLimit: 5,
+    acquireTimeout: 300
+}
+
+class DBConnector {
+    dbconnector = mariadb.createPool(config);
+
+    async query(param) {
+        var conn = await this.dbconnector.getConnection();
+        var ret = null;
+        conn.query(param)
+        .then(data => {
+            ret = data;
+            console.log(data);
+            conn.end()
+        })
+        .catch(err => {
+            console.log(err)
+            conn.end()
+        })
+        return ret;
+    }
+}
+
+//module exports = new DBConnector();
