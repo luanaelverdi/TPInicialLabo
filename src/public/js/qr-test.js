9b0a913ce77a22e5cb3fbb21eb2b9023248c5bd6
luanaelverdi <luelverdi02@gmail.com>
2024-08-24 21:02:55 -0300

    apreto boton generarQR y se vincula al generador

--- src/public/js/qr-test.js
+++ src/public/js/qr-test.js
@@ -1,4 +1,4 @@
-const container = document.createElement('div');
+/*const container = document.createElement('div');
 document.body.appendChild(container);
 
 const qrcode = new QRCode(container, {
@@ -9,3 +9,18 @@ const qrcode = new QRCode(container, {
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
 });
+*/
+
+function generarQR(){
+    const contenedor = document.querySelector('.contenedor-qr');
+    document.body.appendChild(contenedor);
+
+    const qrcode =  new QRCode(contenedor, {
+        text: "https://github.com/luanaelverdi/TPInicialLabo",
+        width: 256,
+        height: 256,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+}
