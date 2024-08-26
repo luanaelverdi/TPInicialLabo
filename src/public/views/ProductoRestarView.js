ab04fd7f1875288bba4a3354b1f244c8b37cfef1
luanaelverdi <luelverdi02@gmail.com>
2024-08-26 16:08:46 -0300

    update interfaz

--- src/public/views/ProductoRestarView.js
+++ src/public/views/ProductoRestarView.js
@@ -22,15 +22,39 @@ export default class extends AbstractView {
 
     pintarProducto (producto) {
         const contenedor = document.getElementById('contenedor-producto');
-        contenedor.innerHTML = JSON.stringify(producto);
-        const input = document.createElement('input');
-        input.type = 'number';
+        //contenedor.innerHTML = JSON.stringify(producto);
+        const inputStock = document.createElement('input');
+        inputStock.type = 'number';
+        inputStock.setAttribute('class', 'form-control');
+        inputStock.style.width= '30%';
+        inputStock.style.marginBottom= '30px';
+        
+        const pStockActual = document.createElement('label');
+        pStockActual.type ='text'; 
+        pStockActual.setAttribute('class', 'form-label');
+        pStockActual.innerHTML = 'Stock actual: ';
+        const pStock = document.createElement('label');
+        pStock.type ='text'; 
+        pStock.setAttribute('class', 'form-label');
+        pStock.innerHTML ='Stock a restar: '
+
+
+        const inputStockActual = document.createElement('input');
+        inputStockActual.type ='number'; 
+        inputStockActual.setAttribute('class', 'form-control');
+        inputStockActual.style.width= '30%';
+        inputStockActual.setAttribute('readonly', 'true');
+        inputStockActual.style.marginBottom= '30px';
 
         const button = document.createElement('button');
         button.textContent = 'Restar';
+        button.setAttribute('class', 'btn btn-primary');
+
+        inputStockActual.value = producto.stock;
 
         button.addEventListener('click', async () => {
-            const value = input.value;
+            const value = inputStock.value;
+            console.log(value)
             const request = await fetch(`/api/producto/${producto.id_producto}/restar/${value}`, { method: "DELETE" });
             const response = await request.json();
             if (!response.ok && response.error.code === "limite") {
@@ -41,15 +65,20 @@ export default class extends AbstractView {
             window.location.reload();
         });
 
-        contenedor.appendChild(input);
+        contenedor.appendChild(pStockActual);
+        contenedor.appendChild(inputStockActual);
+        contenedor.appendChild(pStock);
+        contenedor.appendChild(inputStock);
         contenedor.appendChild(button);
+
     }
 }
 
 const VIEW_CONTENT = `
     <div class="container-view">
     ${TEMPLATE_NAVIGATION}
-        <h1>RESTAR STOCK</h1>
-        <div id="contenedor-producto"></div>
+        <h1 class ="text-start">RESTAR STOCK</h1>
+        <div class = " form-control" id="contenedor-producto">
+        </div>
     </div>
 `;
