2595a5f8332cd6f58b1b9df515caa88cacf2b174
luanaelverdi <luelverdi02@gmail.com>
2024-08-28 22:35:59 -0300

    pantalla login

--- src/public/views/LoginView.js
+++ src/public/views/LoginView.js
@@ -16,8 +16,19 @@ export default class extends AbstractView {
 const VIEW_CONTENT = `
     <div class="container-view">
         ${TEMPLATE_NAVIGATION}
-        <div id="contenedor-login">
-            <h1>Iniciar Sesion</h1>
+        <h1 class ="h1 text-center"> Iniciar Sesion </h1>
+        <div class = "container m-3">
+            <form>
+                <div class="mb-3">
+                    <label for="usuario" class="form-label" placeholder = "Ingrese su usuario">Usuario</label>
+                    <input type="text" class="form-control">
+                </div>
+                    <div class="mb-3">
+                    <label for="contraseña" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" id="contraseña">
+                </div>
+                <button type="submit" class="btn btn-primary">Entrar</button>
+            </form>
         </div>
     </div>
 `;
