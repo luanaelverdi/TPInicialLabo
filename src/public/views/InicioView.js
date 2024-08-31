0727ba5dfb56963e41d084db2b57953dfeb2504c
luanaelverdi <luelverdi02@gmail.com>
2024-08-31 18:08:27 -0300

    img fondo

--- src/public/views/InicioView.js
+++ src/public/views/InicioView.js
@@ -18,7 +18,7 @@ const VIEW_CONTENT = `
         ${TEMPLATE_NAVIGATION}
         
         <div class="card text-bg-dark">
-           <img id="img-fondo-inicio" src="https://img.pikbest.com/wp/202344/light-gray-background-design-abstract-grey-color-dark-black-and-with-old-wall-texture-of-cement-illuminated-by-white-gradient_9898524.jpg!sw800" class="img-fluid" alt="fondo">
+           <img id="img-fondo-inicio" src="https://www.tenvinilo-argentina.com/webp/wallpaper/large/papel-tapiz-ladrillos-ilusion-de-ladrillo-42520.webp" class="img-fluid" alt="fondo">
             <div class="card-img-overlay">
                 <h1 class="display-1 text-center" style="color:black;">BIENVENIDOS A LADRILLITO S.A.</h1>
             </div>
